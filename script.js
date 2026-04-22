820737d9a9abe1eba4069f2b322e8d586dfa59bd
Drakoz <jaygd221@gmail.com>
2026-04-22 10:15:03 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('hideSidebar').addEventListener('click', function() {
+    const sidebar = document.getElementById('sidebar');
+    if (sidebar.style.display === 'none') {
+        sidebar.style.display = 'block';
+    } else {
+        sidebar.style.display = 'none';
+    }
+});
